771891ac1e6eabf5a869d3cc7e65028a05b0a942
abhaykumar181 <abhay@avantgardedigital.com>
2023-10-31 18:54:30 +0530

    splash screen, unfinished login screen work

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, Image, StatusBar, TextInput, Button, View } from "react-native";
+
+const App = () => {
+    return(
+      <SafeAreaView style={splashStyles.container}>
+        <StatusBar
+        animated={true}
+        backgroundColor="#1e3273"
+      />
+        <Image
+          style={splashStyles.appLogo}
+          source={require('./src/assets/images/tastytracks-logo.png')}
+        />
+        <Text style={splashStyles.appTitle}>Login to Tasty Tracks</Text>
+        <TextInput style={splashStyles.textInput} placeholder="Username" />
+        <TextInput style={splashStyles.textInput} placeholder="Password" secureTextEntry={true} />
+        
+        <View style={{padding:10}}>
+          <Button
+            title="Login"
+            color="#1e3273"
+          />
+        </View>
+      </SafeAreaView>
+    );
+}
+
+export default App;
+
+const splashStyles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:"#ddd",
+    justifyContent:"center",
+    paddingHorizontal:10,
+  },
+  appLogo:{
+    resizeMode: 'contain',
+    height: 100,
+    width: 100,
+    alignSelf:"center",
+  },
+  appTitle:{
+    fontSize:25,
+    color:"#1e3273",
+    fontFamily: "Urbanist-Bold",
+    alignSelf:"center",
+  },
+  textInput:{
+    height: 40,
+    margin: 13,
+    borderWidth: 1,
+    padding: 10,
+    borderColor:"#1e3273",
+    borderRadius:5    
+  },
+});
---
+++ src/screens/Splash.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, Image, View, StatusBar } from "react-native";
+
+const App = () => {
+    return(
+      <SafeAreaView style={splashStyles.container}>
+        <StatusBar
+        animated={true}
+        backgroundColor="#1e3273"
+      />
+        <View style={splashStyles.logoContainer}>
+          <Image
+            style={splashStyles.appLogo}
+            source={require('./src/assets/images/tastytracks-logo.png')}
+          />
+          <Text style={splashStyles.appTitle}>Tasty Tracks</Text>
+          <Text style={splashStyles.tagLine}>Blue Apron Unforgettable meals. Delivered.</Text>
+        </View>
+        <View>
+          <Text style={splashStyles.footerText}>Application built by Abhay Kumar</Text>
+        </View>
+      </SafeAreaView>
+    );
+}
+
+export default App;
+
+const splashStyles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:"#1e3273",
+    justifyContent:"center",
+    paddingHorizontal:10,
+  },
+  logoContainer:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  appLogo:{
+    resizeMode: 'contain',
+    height: 100,
+    width: 200,
+  },
+  appTitle:{
+    fontSize:30,
+    color:"#fff",
+    fontFamily: "Urbanist-Bold"
+  },
+  tagLine:{
+    fontSize:18,
+    color:"#fff",
+    fontFamily: "Urbanist-Italic"
+  },
+  footerText:{
+    textAlign:"center",
+    color:"#fff",
+    fontFamily: "Urbanist-Italic",
+    fontSize:16,
+    padding:15,
+  }
+});
